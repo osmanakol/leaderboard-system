4b513faf3dd78e22969775fa5e3c54a4e9d6c301
Osman Akol <osmanakol@outlook.com>
2021-12-30 19:59:10 +0300

    country endpoints connected to api

--- api/src/routes/api.route.ts
+++ api/src/routes/api.route.ts
@@ -1,26 +1,24 @@
 import express from "express";
+import { CountryRoute } from "../lib/country/country.route";
 
 export class ApiRoute {
     protected route: express.Router
+    private _countryRoute:CountryRoute
 
     constructor() {
         this.route = express.Router()
+        this._countryRoute = new CountryRoute()
     }
 
     public Routes = (): express.Router => {
-        this.route.get(
-            "/",
-            (
-              req: express.Request,
-              res: express.Response,
-              next: express.NextFunction
-            ) => {
-              res.status(200).json({
-                status: "success",
-                message: "Api is working",
-              });
-            }
-        );
+        this.route.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+          res.status(200).json({
+            status: "success",
+             message: "Api is working",
+          });
+        });
+
+        this.route.use(this._countryRoute.getPath(), this._countryRoute.getRoutes())
 
         return this.route
     }
