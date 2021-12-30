5b40fd598141bc14b813d6cf428eda30c7fc8f0b
Osman Akol <osmanakol@outlook.com>
2021-12-30 20:56:03 +0300

    added throw error

--- api/src/lib/country/country.controller.ts
+++ api/src/lib/country/country.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import IController from '../../interfaces/IController';
 import { CountryService } from './country.service';
-import { HttpStatusCode, BaseResponse } from "../../utils/index";
+import { HttpStatusCode, BaseResponse, HTTP400Error } from "../../utils/index";
 
 export class CountryController implements IController {
     private _countryService : CountryService
@@ -16,12 +16,18 @@ export class CountryController implements IController {
             lean: true
         })
 
+        if (!countries.length)
+            throw new HTTP400Error("Countries data could not get")
+
         BaseResponse(HttpStatusCode.OK, countries, res)
     }
 
     public getCountry =async (req:Request, res:Response, next:NextFunction) => {
 
         const country = await this._countryService.findOne({country_id: req.params.id}, {_id:0}, {lean:true})
+
+        if (country == null)
+            throw new HTTP400Error("Country could not found")
 
         BaseResponse(HttpStatusCode.OK, country, res)
     }
