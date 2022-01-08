e95310cd770d47207774baf2fe0fecdb0d6c385c
Osman Akol <osmanakol@outlook.com>
2022-01-08 21:36:00 +0300

    implemented redis

---
+++ services/rank_service/src/utils/redis.util.ts
@@ -0,0 +1,84 @@
+import Redis, {ClusterOptions} from "ioredis";
+import { REDIS_OPTIONS } from "../config";
+
+class RedisUtil {
+    public redis_instance:Redis.Cluster; 
+
+    constructor() {
+        this.redis_instance = new Redis.Cluster([
+            {
+                port: 7000,
+                host: "127.0.0.1"
+            },
+            {
+                port: 7001,
+                host: "127.0.0.1"
+            },
+            {
+                port: 7002,
+                host: "127.0.0.1"
+            },
+            {
+                port: 7003,
+                host: "127.0.0.1"
+            },
+            {
+                port: 7004,
+                host: "127.0.0.1"
+            },
+            {
+                port: 7005,
+                host: "127.0.0.1"
+            },
+        ], {
+            scaleReads: "slave",
+            redisOptions: REDIS_OPTIONS
+        })
+        this.connect_redis()
+    }
+    
+    private connect_redis = async () => {
+        await this.redis_instance.connect()
+    }
+
+    protected disconnect_redis = async () => {
+        await this.redis_instance.disconnect()
+    }
+
+    public getSet = async (set_name:string): Promise<String | null> => {
+        return await this.redis_instance.get(set_name)
+    }
+
+    public addSet = async (set_name: string, data:any, expire_mode:string = "", expire_time:number = 0) => {
+        const result = await this.redis_instance.set(set_name, JSON.stringify(data))
+        console.log(result)
+    }
+
+    public addSortSet = async (set_name: string, set_key: string, set_value:number) => {
+        await this.redis_instance.zadd(set_name, set_value, set_key)
+    }
+
+    public getScore = async (set_name: string, set_key:string):Promise<string | any> => {
+        return await this.redis_instance.zscore(set_name, set_key)
+    }
+
+    public getRank = async (set_name: string, set_key: string): Promise<Number | null> => {
+        return await this.redis_instance.zrevrank(set_name, set_key)
+    }
+
+    public getRankViaRange = async (set_name:string, start_index: number, stop_index: number): Promise<String[]> => {
+        return await this.redis_instance.zrevrange(set_name, start_index, stop_index, "WITHSCORES")
+    }
+
+    public increaseScore = async (set_name: string, set_key: string, value: number ): Promise<String> => {
+        return await this.redis_instance.zincrby(set_name, value, set_key)
+    }
+
+    public checkSortedSet = async (set_name: string): Promise<Number> => {
+        return await this.redis_instance.zcard(set_name)
+    }
+
+}
+
+
+export default new RedisUtil()
