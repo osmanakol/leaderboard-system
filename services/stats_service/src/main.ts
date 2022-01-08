9aabb1577e79abab846fcd07d32584a81c66d58b
Osman Akol <osmanakol@outlook.com>
2022-01-08 21:30:46 +0300

    implemented main file

---
+++ services/stats_service/src/main.ts
@@ -0,0 +1,33 @@
+import * as grpc from '@grpc/grpc-js';
+import { StatsService } from '../protos/build/stats_service/stats_service_grpc_pb';
+import { StatController } from './model/stats.model/stats.controller';
+import mongo_connection from "./database/mongo.database";
+import { STAT_HOST, STAT_PORT } from "./config";
+
+
+const mongoSetup = async () => {
+    try {
+        await mongo_connection.connection()
+    } catch (error) {
+        throw error
+    }
+}
+
+
+const serve = () => {
+    const server = new grpc.Server()
+
+    // @ts-ignore
+    server.addService(StatsService, new StatController())
+    server.bindAsync(`${STAT_HOST}:${STAT_PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+        if (err)
+            throw err
+
+        console.log(`Listening port ${port}`)
+        
+        server.start()
+    })
+}
+
+mongoSetup()
+serve()
