b01c45cb083cd5b5c1aac20215d6a2da43b743af
Osman Akol <osmanakol@outlook.com>
2022-01-08 21:21:12 +0300

    implemented database connection, interfaces and repository pattern

---
+++ services/pool_service/src/database/mongo.database.ts
@@ -0,0 +1,39 @@
+import { IConnection } from "./IConnection";
+import { MONGO_CONNECT_OPTIONS, MONGODB_URI } from "../config";
+import mongoose from "mongoose";
+
+class MongooseConnection implements IConnection {
+    private uri:string=MONGODB_URI
+
+    public async connection(): Promise<void> {
+        mongoose.Promise = global.Promise
+
+        mongoose.connect(this.uri, MONGO_CONNECT_OPTIONS)
+
+        mongoose.connection.on("error", (err) => {
+            //loggerUtil.error("Failed to Connect MongoDb", err)
+            console.error("Failed to Connect MongoDb")
+        })
+
+        mongoose.connection.on("disconnected", (err) => {
+            //loggerUtil.warn("Mongo db disconnected", err)
+            console.warn("Mongo db disconnected")
+        })
+
+        mongoose.connection.on("connected", (err) => {
+            //loggerUtil.info("mongo db is connected", err)
+            console.info("mongo db is connected")
+        })
+        
+    }
+    disconnection(): void {
+        mongoose.connection.close(() => {
+            //loggerUtil.info("Mongoose default connection is disconnected")
+            console.info("Mongoose default connection is disconnected")
+        })
+    }
+
+}
+
+
+export default new MongooseConnection()
---
+++ services/pool_service/src/repository/base/BaseRepository.ts
@@ -0,0 +1,35 @@
+import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types';
+import { Aggregate, FilterQuery, PipelineStage, Query, QueryOptions, SaveOptions, UpdateQuery } from 'mongoose';
+import { IRead } from '../interface/IRead';
+import { IWrite } from '../interface/IWrite';
+
+export abstract class BaseRepository<T, K> implements IRead<T, K>, IWrite<T,K> {
+    protected readonly _model:ModelType<K>
+
+    constructor(model: ModelType<K>) {
+        this._model = model;
+    }
+
+    
+    async create(query: Query<T, K, {}, K>, options?: SaveOptions): Promise<DocumentType<K>> {
+        return await new this._model(query).save(options) as DocumentType<K>
+    }
+    async update(query: FilterQuery<T>, update: UpdateQuery<T>, options: QueryOptions): Promise<Boolean> {
+        return await (await this._model.updateOne(query, update, options)).modifiedCount != 0
+    }
+    async delete(query: FilterQuery<T>, options?: QueryOptions): Promise<Boolean> {
+        return await (await this._model.deleteOne(query, options)).deletedCount != 0
+    }
+    async find(args: FilterQuery<T>, projection?: any, options?: QueryOptions): Promise<DocumentType<K>[]> {
+        return await this._model.find(args, projection, options) as DocumentType<K>[]
+    }
+    async findOne(args: FilterQuery<T>, projection: any, options: QueryOptions): Promise<DocumentType<K> | null> {
+        return await this._model.findOne(args, projection, options) as DocumentType<K>
+    }
+
+    async aggregation(pipeline: PipelineStage[]) {
+        return await this._model.aggregate(pipeline, {})
+    }
+
+    
+}
