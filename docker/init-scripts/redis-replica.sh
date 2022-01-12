757d8668b6f3be8c7942221e20efbc0ca74ec640
Osman Akol <osmanakol@outlook.com>
2022-01-12 23:32:15 +0300

    added redis replication script

---
+++ docker/init-scripts/redis-replica.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+redis-server /etc/redis/redis.conf
+
+redis-cli --cluster create \
+10.10.10.10:6379 \
+10.10.10.11:6379 \
+10.10.10.12:6379 \
+10.10.10.13:6379 \
+10.10.10.14:6379 \
+10.10.10.15:6379 \
+--cluster-replicas 1
