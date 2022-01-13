b7b8b42db69a64b87e9f3cb83c8aed5cb023bf7c
Osman Akol <osmanakol@outlook.com>
2022-01-13 13:32:52 +0000

    build proto script changed according to docker container requirements

--- scripts/build-protos.sh
+++ scripts/build-protos.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-SERVICES=$(pwd)/services/
+
+HOME=$(pwd)
+#SERVICES=services/$1
 
 
-PROTO_DEST="$SERVICES"/$1/
+#PROTO_DEST="$SERVICES"/$1/
 
-cd "$PROTO_DEST"
+#cd "$PROTO_DEST"
 
-PROTO_BUILD_DEST="$SERVICES"/$2/protos/build/$1/
+PROTO_BUILD_DEST="$HOME"/protos/build/
 
 mkdir -p "${PROTO_BUILD_DEST}"
 
-grpc_tools_node_protoc \
-    --plugin=protoc-gen-ts="$PROTO_DEST"/node_modules/.bin/protoc-gen-ts \
+node_modules/.bin/grpc_tools_node_protoc \
+    --plugin=protoc-gen-ts=node_modules/.bin/protoc-gen-ts \
     --ts_out=grpc_js:"${PROTO_BUILD_DEST}" \
     --js_out=import_style=commonjs,binary:"${PROTO_BUILD_DEST}" \
     --grpc_out=grpc_js:"${PROTO_BUILD_DEST}" \
     -I ./protos \
     protos/*.proto
+
+#ls -la $PROTO_BUILD_DEST
+npm run build
+npm run prod
